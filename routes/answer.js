9c683e5e7bb437f988d63857c94c87fdc09d2d8f
alaqrasenpai <52831910+alaqrasenpai@users.noreply.github.com>
2022-09-28 09:39:16 +0300

    add link

--- routes/answer.js
+++ routes/answer.js
@@ -3,6 +3,7 @@ const router = require("express").Router(),
 
 router.get("/", AnswerController.index)
 router.get("/search", AnswerController.show)
+router.get("/searchbyqid", AnswerController.showByQuastionId)
 router.put("/update", AnswerController.update)
 router.delete("/delete", AnswerController.delete)
 router.post("/create", AnswerController.create)
